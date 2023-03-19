bc022e8eab9675e4ff6a2fbc5cf9cbe09e958512
EngrPips <95760918+EngrPips@users.noreply.github.com>
2023-03-19 00:25:00 -0700

    created  a new nft collection
    it has a whitelist contract and then a mint contract coupled with a front end

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7585C6153Da82814bD91b7e2c4EC3FAFafB1fDD9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
