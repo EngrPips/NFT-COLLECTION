9c531228d42a2845c1dfbf604eb35d0ae54b34c2
EngrPips <95760918+EngrPips@users.noreply.github.com>
2023-03-19 01:20:48 -0700

    updated the metadata url
    updating the metadata url required me to create a new fil
    r named [tokenId].js and also required me to redeploy my nft contract as i need to make sure my metdata url is pointing to the  newly crea
    ted file that holds it

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x7585C6153Da82814bD91b7e2c4EC3FAFafB1fDD9";
+const WHITELIST_CONTRACT_ADDRESS = "0x2Fb9BD236eE73cB88EBAc5d44E7a53B80107d9AC";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-swart-omega.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
